61b70208ea546540da582c55538b3e43d2931307
Evgenii Kazakov <evgenii.kazakov@aimtech.com>
2018-07-30 19:52:13 +0300

    common: inital commit

---
+++ base_solution/solution.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <sstream>
+#include <atomic>
+#include <memory>
+#include <cassert>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+constexpr size_t kMaxThreadsCount = 15;
+
+#define log(log_message, method_name, class_name)\
+  clog << '[' << class_name << "::" << method_name << "]: " << log_message << endl;
+
+class Solution {
+protected:
+  explicit Solution(size_t case_number):
+      case_number_(case_number) {
+    read_input();
+  }
+
+  void read_input() {}
+  void solve_internal() {}
+  void print_result() {}
+
+  size_t case_number_;
+};
+
+class AsyncSolution : Solution {
+public:
+  AsyncSolution(size_t case_number, atomic<size_t>& free_threads_count):
+      Solution(case_number),
+      free_threads_count_(free_threads_count) {}
+
+  void start_solving_thread() {
+    assert(free_threads_count_ > 0);
+    --free_threads_count_;
+    solving_thread_ = make_unique<thread>([this]{this->solve();});
+  }
+
+private:
+  void solve() {
+    log("start solution " << case_number_, "solve", "AsyncSolution");
+    solve_internal();
+    log("finish solution " << case_number_, "solve", "AsyncSolution");
+    ++free_threads_count_;
+  }
+
+  atomic<size_t>& free_threads_count_;
+  unique_ptr<thread> solving_thread_;
+};
+
+int main() {
+  atomic<size_t> free_threads_count = kMaxThreadsCount;
+
+  size_t case_count;
+  cin >> case_count;
+  vector<AsyncSolution> solutions;
+  for (size_t i = 0; i < case_count; ++i) {
+    solutions.emplace_back(i, free_threads_count);
+  }
+  log("solutions initialized", "main", "");
+
+  for (auto& solution : solutions) {
+    solution.start_solving_thread();
+    while (free_threads_count == 0) {}
+  }
+  log("all solutions finished", "main", "");
+}
---
+++ compile_solution.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir bin
+g++ base_solution/solution.cpp -o bin/solution -std=c++17 -pthread
