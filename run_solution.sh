baa9c1ae1d32c734719343b547ec58e10532e796
Evgenii Kazakov <evgenii.kazakov@aimtech.com>
2018-07-30 23:05:59 +0300

    run_problem: add always build before run

--- run_solution.sh
+++ run_solution.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
 if [ "$#" -ne 2 ]; then
     echo "Usage: ./run_solution.sh <problem> <test>"
     exit
 fi
 
+echo "build solution"
+./build.sh $1
+echo "start solution"
 time problems/$1/build/launch < problems/$1/tests/$2.txt > problems/$1/answers/$2.ans
 
 echo "----------------------------"
